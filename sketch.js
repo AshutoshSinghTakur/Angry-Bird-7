ce0656f5a4d47f18fad85c60375dd45af5882156
AshutoshSinghTakur <ashuthakur1718@gmail.com>
2021-07-02 17:06:58 +0530

    smoke

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState = "ON_SLING"
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,11 +70,12 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState === "ON_SLING"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
-
-
 function mouseReleased(){
+    gameState = "RELEASED"
     slingshot.fly();
 }
 
